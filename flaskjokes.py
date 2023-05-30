6ead45109906e4674fe98aae8dd569d47fb847c3
Sergei Shcherbak <sshch1987@gmail.com>
2023-05-31 00:37:56 +0300

    Update flaskjokes.py

--- flaskjokes.py
+++ flaskjokes.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 
-from pyjoke import whysoserious
+from pyjoke import whysoserious, warumsoernst
 
 
 app = Flask(__name__)
@@ -9,7 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("flaskjokes.html")
+    Joker = whysoserious()
+    Witzbold = warumsoernst()
+    return render_template("flaskjokes.html", Joker, Witzbold)
 
 
 if __name__ == '__main__':
